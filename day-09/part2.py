946750acc5b914f94bdabc27ef82c86bf6e9085b
Chris Dean <chrisdean258@gmail.com>
2023-12-09 00:08:05 -0500

    day 9

---
+++ day-09/part2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def predict(nums):
+    diffs = [b - a for (a, b) in zip(nums, nums[1:])]
+    if all(d == 0 for d in diffs):
+        return nums[-1]
+    ld = predict(diffs)
+    return nums[0] - ld
+
+
+s = 0
+for line in open(0):
+    nums = [int(a) for a in line.split()]
+    print(predict(nums))
+    s += predict(nums)
+print(s)
+
