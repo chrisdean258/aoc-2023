7a61e2d0361402df86760c3f8a3e4bb56cf44b8d
Chris Dean <chrisdean258@gmail.com>
2023-12-26 02:31:41 -0500

    day 24 optimizations

--- day-24/part1.py
+++ day-24/part1.py
@@ -2,47 +2,31 @@
 
 from fractions import Fraction
 
-import numpy as np
-
 drops = []
 for line in open(0).read().splitlines():
     line = line.replace(",", "").replace("@", "")
     drops.append([int(a) for a in line.split()])
 
-
-def std(x, y, vx, vy):
-    m = Fraction(vy, vx)
-    return (-m, Fraction(1)), y - m * x
-
-
-def when(p, x, y, dx, dy):
-    return Fraction(p[0] - x, dx)
-
-
-def solve(a, c):
-    den = a[0][0] * a[1][1] - a[0][1] * a[1][0]
-    if den == 0:
-        return None
-    xnum = c[0] * a[1][1] - a[0][1] * c[1]
-    ynum = c[1] * a[0][0] - a[1][0] * c[0]
-    return np.array([xnum / den, ynum / den])
-
+lb = 200000000000000
+ub = 400000000000000
+# lb = 7
+# ub = 27
 
 s = 0
 for i, drop in enumerate(drops):
-    x, y, _, vx, vy, _ = drop
-    std1, std13 = std(x, y, vx, vy)
+    x1, y1, _, vx1, vy1, _ = drop
     for other_drop in drops[i:]:
-        xx, yy, _, vxx, vyy, _ = other_drop
-        std2, std23 = std(xx, yy, vxx, vyy)
-        a = np.array([std1, std2])
-        b = np.array([std13, std23])
-        p = solve(a, b)
-        if p is None:
+        x2, y2, _, vx2, vy2, _ = other_drop
+        den = vx2*vy1 - vy2*vx1
+        if den == 0:
             continue
-        if not ((200000000000000 <= p).all() and (400000000000000 >= p).all()):
+        num = vy2 * vx1 * (x1 - x2) + vx2 * vx1 * (y2 - y1) + x1 * den
+        x = Fraction(num, den)
+        t1 = (x - x1) / vx1
+        t2 = (x - x2) / vx2
+        if t1 < 0 or t2 < 0:
             continue
-        t1 = when(p, x, y, vx, vy)
-        if t1 >= 0:
-            s += when(p, xx, yy, vxx, vyy) >= 0
+        y_1 = y1 + t1 * vy1
+        s += lb <= x <= ub and lb <= y_1 <= ub
+
 print(s)
