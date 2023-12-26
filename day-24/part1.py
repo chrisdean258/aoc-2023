fe5178839c9dc4c1131d381fe23efe2953265fe8
Chris Dean <chrisdean258@gmail.com>
2023-12-26 02:40:45 -0500

    no more fractions and we're under .1 seconds

--- day-24/part1.py
+++ day-24/part1.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from fractions import Fraction
 
 drops = []
 for line in open(0).read().splitlines():
@@ -9,24 +8,42 @@ for line in open(0).read().splitlines():
 
 lb = 200000000000000
 ub = 400000000000000
-# lb = 7
-# ub = 27
 
 s = 0
 for i, drop in enumerate(drops):
     x1, y1, _, vx1, vy1, _ = drop
     for other_drop in drops[i:]:
         x2, y2, _, vx2, vy2, _ = other_drop
-        den = vx2*vy1 - vy2*vx1
-        if den == 0:
+        xden = vx2*vy1 - vy2*vx1
+        if xden == 0:
             continue
-        num = vy2 * vx1 * (x1 - x2) + vx2 * vx1 * (y2 - y1) + x1 * den
-        x = Fraction(num, den)
-        t1 = (x - x1) / vx1
-        t2 = (x - x2) / vx2
-        if t1 < 0 or t2 < 0:
+        xnum = vy2 * vx1 * (x1 - x2) + vx2 * vx1 * (y2 - y1) + x1 * xden
+        if xden < 0:
+            xden = -xden
+            xnum = -xnum
+
+        t1n = xnum - x1 * xden
+        t1d = vx1 * xden
+
+        if t1d < 0:
+            t1d = -t1d
+            t1n = -t1n
+
+        t2n = xnum - x2 * xden
+        t2d = vx2 * xden
+        if t2d < 0:
+            t2d = -t2d
+            t2n = -t2n
+
+        if t1n < 0 or t2n < 0:
             continue
-        y_1 = y1 + t1 * vy1
-        s += lb <= x <= ub and lb <= y_1 <= ub
+
+        y_1n = y1 * t1d + t1n * vy1
+        y_1d = t1d
+        if y_1d < 0:
+            y_1d = -y_1d
+            y_1n = -y_1n
+
+        s += lb * xden <= xnum <= ub * xden and lb * y_1d <= y_1n <= ub * y_1d
 
 print(s)
